a8b2c5592303fada25034839e79d3ef4224d7c18
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-06-04 09:12:52 +0900

    'data'

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+// 기본 라우트
+app.get("/", (_: Request, res: Response) => {
+  res.json({
+    message: "TypeScript Express 서버가 정상적으로 실행 중입니다!",
+  });
+});
+
+// 서버 시작
+app.listen(3000, () => {
+  console.log(`서버가 http://localhost:3000 에서 실행 중입니다`);
+});
